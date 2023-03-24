341118cd222e72bee3f8585eca974dbf09e603fb
Austin McCormick <121597363+austindan5@users.noreply.github.com>
2023-03-24 11:00:08 -0400

    Add files via upload
    March 24-23 Coding Challenge
    8 kyu
    Keep up the hoop

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-// Given a non-empty array of integers, return the result of multiplying the values together in order.
-// Example: [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+// Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
 
-function grow(x){
-    x.reduce((a,b) => a*b, 1)
-} 
+// Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+
+// If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+// If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+function hoopCount (n) {
+    if (n>=10)  {
+      return "Great, now move on to tricks"
+    } else {
+      return "Keep at it until you get it"
+    }
+ }
