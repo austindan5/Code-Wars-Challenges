cba94a9a2a1a997c19f686ddcbe4097c87e74cdb
Austin McCormick <121597363+austindan5@users.noreply.github.com>
2023-04-27 12:40:35 -0400

    Add files via upload

---
+++ Debug sayHello/main.js
@@ -0,0 +1,18 @@
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+function sayHello(name) {
+  return `Hello, ${name}`;
+}
+
+// Prep
+// Parameters-name
+// Return-"Hello, ${name}"
+// Examples-sayHello(Mr. Spoke)"Hello, Mr. Spock"
+// PsuedoCode
+//Want code to take in name
+//then place it in a string with Hello
+//then return Hello ${name}
