ba61f33f54f1ff20fa9efe865d3707682cd404f7
Austin McCormick <121597363+austindan5@users.noreply.github.com>
2023-05-03 10:31:03 -0400

    Add files via upload

---
+++ Even or Odd with a twist/main.js
@@ -0,0 +1,19 @@
+function evenOrOdd(number) {
+    if(number%2===0){
+      return "Even"
+    }else{
+      return "Odd"
+    }
+  }
+  
+  //PREP
+  //Parameters-an integer
+  //Returns-Even or Odd strings
+  //1=>odd 64=> even
+  //Pseudo Code
+  //conditional
+  //if number %2===
+  //no remainder
+
+  //DESCRIPTION:
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
