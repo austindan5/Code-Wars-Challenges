c5ce2f82331703203e6d1a18d47afd05954e5f9e
Austin McCormick <121597363+austindan5@users.noreply.github.com>
2023-05-09 12:41:19 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,14 @@
-// To find the volume (centimeters cubed) of a cuboid you use the formula:
-
-// volume = Length * Width * Height
-
-// But someone forgot to use proper record keeping, so we only have the volume, and the length of a single side!
-
-// It's up to you to find out whether the cuboid has equal sides (= is a cube).
-
-// Return true if the cuboid could have equal sides, return false otherwise.
-
-// Return false for invalid numbers too (e.g volume or side is less than or equal to 0).
-
-// Note: side will be an integer
-var cubeChecker = function (volume, side) {
-  if (side ** 3 !== volume) {
-    return false;
-  } else if (volume <= 0 || side <= 0) {
-    return false;
-  } else {
-    return true;
+function sumArray(array) {
+    if (array == null) {
+      return 0;
+    } else if (array.length < 2) {
+      return 0;
+    } else {
+      array = array.sort(function(a,b) {return a - b;});
+      var total = 0;
+      for (var i = 1; i < array.length - 1; i++) {
+        total += array[i];
+      }
+      return total;
+    }
   }
-};
-
-// To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. This function must return another function, which takes a single integer as an argument and returns a new array.
-
-// The returned array should consist of each of the elements from the first array multiplied by the integer.
-
-// Example:
-
-// multiplyAll([1, 2, 3])(2) = [2, 4, 6];
-// You must not mutate the original array.
-
-function multiplyAll(arr) {
-  return function (n) {
-    return arr.map((x) => x * n);
-  };
-}
